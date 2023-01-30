a1f672db8760c8ac097fc87e6574f75332421250
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-01-30 14:12:02 +0100

    📦 NEW: add Text and StyledText components

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,10 +1,31 @@
 import { ReactNode } from 'react'
-import { StyledText } from './styled'
+import { TextVariants, ThemeColors, ThemeFontSizes } from '../../style/theme'
+import { StyledText, StyledTextProps } from './styled'
 
-type Props = {
+type Props = Partial<Omit<StyledTextProps, 'as'>> & {
   children: ReactNode
+  variant?: TextVariants
 }
 
-export const Text = ({ children }: Props) => {
-  return <StyledText>{children}</StyledText>
+export const Text = ({
+  children,
+  variant = 'p',
+  size,
+  color = 'textDark',
+  lineHeight = 1.2,
+  bold = false,
+  uppercase = false
+}: Props) => {
+  return (
+    <StyledText
+      as={variant}
+      size={size}
+      color={color}
+      lineHeight={lineHeight}
+      bold={bold}
+      uppercase={uppercase}
+    >
+      {children}
+    </StyledText>
+  )
 }
--- src/components/Text/styled.ts
+++ src/components/Text/styled.ts
@@ -1,6 +1,27 @@
 import styled from 'styled-components'
+import { theme, TextVariants, ThemeColors } from '../../style/theme'
 
-export const StyledText = styled.p`
-  color: ${(props) => props.theme.colors.primary};
-  font-size: ${() => 2 * 1}rem;
+export type StyledTextProps = {
+  as: TextVariants
+  size?: number
+  color: ThemeColors
+  lineHeight: number
+  bold: Boolean
+  uppercase?: Boolean
+}
+
+export const StyledText = styled.p<StyledTextProps>`
+  color: ${({ theme, color }) => theme.colors[color]};
+  font-size: ${({ theme, as, size }) => {
+    if (size) {
+      return size
+    } else {
+      const fontSize = theme.textVariants[as].fontSize
+      return theme.fontSizes[fontSize]
+    }
+  }}rem;
+  line-height: ${({ theme, as, lineHeight }) =>
+    lineHeight ? lineHeight : theme.textVariants[as].lineHeight};
+  font-weight: ${({ bold }) => (bold ? 700 : 400)};
+  text-transform: ${({ uppercase }) => (uppercase ? 'uppercase' : 'none')};
 `
